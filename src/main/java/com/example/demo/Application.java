e05ad88dc77714831f878a8054e46eeaea2187a0
varshi-b <varshib004@gmail.com>
2025-11-13 19:18:06 +0530

    feat: Completed initial Spring Boot JPA entity and repository setup

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student varshi =new Student(
+                    "varshi","Bandi","varshi.bandi@gmail.com",21
+            );
+            studentRepository.save(varshi);
+        };
     }
 
 }
